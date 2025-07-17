7201d28e76d0289cf5de7671061b815a740de952
omlan93 <2105162@ugrad.cse.buet.ac.bd>
2025-07-18 02:03:09 +0600

    complete other operations with plus

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let secondOperand = {
 };
 
 let equalPressed = false;
+let ansUpdate = false;
+
+const sign = document.querySelector(".sign");
+
 
 // Helper function to fix floating-point precision issues
 const fixPrecision = (num) => {
@@ -37,43 +41,118 @@ const showOnDisplay = function (e) {
         secondOperand.value = 0;
         firstOperand.state = false;
         secondOperand.state = false;
+        ansUpdate = false;
+    }
+
+    else if (e.target.textContent === "ANS") {
+        display.textContent = "ANS";
     }
 
     else if (e.target.textContent === "=") {
         equalPressed = true;
+        if (display.textContent === "ANS" && signPressed === false) {
+            display.textContent = ans
+        }
 
         if ((firstOperand.state === true) && (signPressed === false)) {
-            secondOperand.value = parseFloat(display.textContent)
+            if (display.textContent === "ANS") {
+                secondOperand.value = ans;
+                console.log("entered ans");
+
+            }
+            else {
+                secondOperand.value = parseFloat(display.textContent);
+            }
             secondOperand.state = true;
             console.log(firstOperand.value);
             console.log(secondOperand.value);
             console.log(typeof (firstOperand.value));
             console.log(typeof (secondOperand.value));
-            const result = fixPrecision(firstOperand.value + secondOperand.value);
-            display.textContent = result;
+            if (signType === "+") {
+                const result = fixPrecision(firstOperand.value + secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "-") {
+                const result = fixPrecision(firstOperand.value - secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "x") {
+                const result = fixPrecision(firstOperand.value * secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "/") {
+                const result = fixPrecision(firstOperand.value / secondOperand.value);
+                display.textContent = result;
+            }
             firstOperand.state = false;
+            ans = parseFloat(display.textContent);
         }
-        ans = parseFloat(display.textContent);
+
 
     }
 
-    else if (e.target.textContent === "+") {
+    else if (((signPressed === true) || (display.textContent === '')) && (e.target.textContent === "-")) {
+        display.textContent = '-';
+        signPressed = false;
+        equalPressed = false;
+    }
+
+    else if (e.target.textContent === "%") {
+        const result = fixPrecision(parseFloat(display.textContent) / 100);
+        display.textContent = result;
+        ans = result;
+    }
+
+    else if ((e.target.textContent === "+") || (e.target.textContent === "-") || (e.target.textContent === "x") || (e.target.textContent === "/")) {
+
+        sign.textContent = e.target.textContent;
+
 
         if ((firstOperand.state === true) && (signPressed === false)) {
-            secondOperand.value = parseFloat(display.textContent)
-            secondOperand.state = true;
+            if (display.textContent === "ANS") {
+                secondOperand.value = ans;
+                console.log("entered ans");
 
-            const result = fixPrecision(firstOperand.value + secondOperand.value);
-            display.textContent = result;
+            }
+            else {
+                secondOperand.value = parseFloat(display.textContent);
+            }
+            secondOperand.state = true;
+            if (signType === "+") {
+                const result = fixPrecision(firstOperand.value + secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "-") {
+                const result = fixPrecision(firstOperand.value - secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "x") {
+                const result = fixPrecision(firstOperand.value * secondOperand.value);
+                display.textContent = result;
+            }
+            else if (signType === "/") {
+                const result = fixPrecision(firstOperand.value / secondOperand.value);
+                display.textContent = result;
+            }
+
             signPressed = false;
+            ans = parseFloat(display.textContent);
         }
 
-        signType = "+";
-        firstOperand.value = parseFloat(display.textContent);
+        signType = e.target.textContent;
+        if (display.textContent === "ANS") {
+            firstOperand.value = ans;
+            console.log("entered ans");
+
+        }
+        else {
+            firstOperand.value = parseFloat(display.textContent);
+        }
         firstOperand.state = true;
         console.log(firstOperand.value);
         console.log(secondOperand.value);
         signPressed = true;
+
     }
 
     else {
